6f7feb36af74b5f2fda6c465b839c5d950623a24
DarkSquirrelComes <darksteellord@gmail.com>
2020-10-17 10:30:30 +0000

    feat: recycle cleaning added

--- week2/srm.sh
+++ week2/srm.sh
@@ -2,11 +2,13 @@
 
 recycle=~/RECYCLE
 
+#check if ~/RECYCLE exists
 if ! [ -e $recycle ]
 then
 	mkdir $recycle
 fi
 
+#move file to ~/RECYCLE
 if [ -e $1 ]
 then
 	mv $1 $recycle
@@ -14,3 +16,7 @@ else
 	echo "No such file"
 fi
 
+#delete old files
+for filename in `find $recycle/* -cmin +10080`; do
+	rm $filename
+done
